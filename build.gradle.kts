1fe6f309536312647ef8bdde5392c920d7519c62
Bruhdows <bruhdows@gmail.com>
2024-11-28 01:19:48 +0100

    fix(jar): Fixed jar building (missing main file)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "me.bruhdows.flappybird.GameMain"
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
